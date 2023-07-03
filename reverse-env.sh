4b0be6bb0d6cd06f976bfea29f07663081c30b16
bigs111 <128871502+bigs111@users.noreply.github.com>
2023-07-03 12:32:51 -0400

    CREATED reverse-env.sh

---
+++ reverse-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# reverse-env.sh
+# this script it ment to be run on a Ubuntu VM
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install terminator -y
+sudo snap install sublime-text --classic
+sudo snap install powershell --classic
+sudo snap install ghidra
+sudo snap install dbeaver-ce
+sudo snap install john-the-ripper
+
+
+
+
+
