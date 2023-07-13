1eba517cf4563669a202a15be7c1aba97e5096b1
bigs111 <128871502+bigs111@users.noreply.github.com>
2023-07-13 10:53:17 -0400

    updated kalicontainer.sh

--- Debian-Based/Kali/kalicontainer.sh
+++ Debian-Based/Kali/kalicontainer.sh
@@ -5,37 +5,14 @@
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-# Installing Dependencies
-echo -e "${GREEN}Installing Dependencies... ${ENDCOLOR}"
+echo -e "${GREEN}Configuring Kali Container...${ENDCOLOR}"
 sudo apt update && sudo apt upgrade -y
-sudo apt install git -y
-sudo apt install wget -y
-sudo apt install curl -y
-sudo apt install micro -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo apt install netutils -y
-sudo apt install build-essential -y
-
-# Installing Tools
-echo -e "${GREEN}Installing Tools... ${ENDCOLOR}"
-sudo apt install nmap -y
-sudo apt install sublist3r -y
-sudo apt install gobuster -y
-sudo apt install hydra -y
-sudo apt install john -y
-sudo apt install wpscan -y
-sudo apt install wordlists -y
-sudo apt install netcat -y
-sudo apt install skipfish -y
-sudo apt install nikto -y
-sudo apt install beef -y
-sudo apt install metasploit-framework -y
-sudo apt install sqlmap -y
+sudo apt install kali-linux-headless -y # <~ kali metapackage for installing kali on a headless machine.
+sudo apt install gobuster -y # <~ directory buster written in go
 
 # Copying wordlists to /home/kali/
 cd
-cp /etc/share/wordlists /home/kali/
+cp -r /etc/share/wordlists /home/kali/def-wordlists
 
 # cloning SecLists
 cd
